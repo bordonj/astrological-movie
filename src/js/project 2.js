cf6d95fd616e8546616e2b6040dae10da99ea2fe
Gurshan Kaile <gurshankaile206@gmail.com>
2021-07-07 13:13:14 -0700

    add function to get Zodiac Elements
    Co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>
    Co-authored-by: Jenn B <jennifer.bordon@gmail.com>
    co-authored-by: Kevin Funez <kevin.funez315@gmail.com>
    Co-Authored-By: Godfrey Owidi <godfreyowiidi@gmail.com>

---
+++ src/js/project 2.js
@@ -0,0 +1,7 @@
+// export function Project() {
+// }
+
+// example commits for the following:
+  // Good Fail: Add first test and Example Class.
+  // Passing: Add constructor logic, pass first test.
+
