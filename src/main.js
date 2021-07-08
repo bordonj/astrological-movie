e96787322ea1cb8d31290ebaf158e874257446a0
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-08 10:53:26 -0700

    add logic to main js file and css styling
    Co-authored-by: Jenn B <jennifer.bordon@gmail.com>
    Co-Authored-By: Godfrey Owidi <godfreyowiidi@gmail.com>
    co-authored-by: Kevin Funez <kevin.funez315@gmail.com>
    co-authored-by: Gurshan Kaile <gurshankaile206@@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,83 +2,50 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Movie from './js/movie-service';
+// import Movie from './js/movie-service';
 import Zodiac from './js/astrology-service';
+console.log('ZODIAC KEY', process.env.ZODIAC_API_KEY);
+// async function getZodiacElements(zodiacSign) {
+//   const response = await Zodiac.fetchData(zodiacSign);
 
-async function getZodiacElements(zodiacSign) {
-  const response = await Zodiac.fetchData(zodiacSign);
+//   let color = response.data.color;
+//   let compatibility = response.data.compatibility;
+//   let luckyTime = response.data.lucky_time;
+//   let description = response.data.description;
+//   let date = response.data.date_range;
+//   let luckyNum = response.data.lucky_number;
+//   let mood = response.data.mood;
 
-  let color = response.data.color;
-  let compatibility = response.data.compatibility;
-  let luckyTime = response.data.lucky_time;
-  let description = response.data.description;
-  let date = response.data.date_range;
-  let luckyNum = response.data.lucky_number;
-  let mood = response.data.mood;
-
-  $(`#color`).text(color);
-  $(`#compatibility`).text(compatibility);
-  $(`#time`).text(luckyTime);
-  $(`#description`).text(description);
-  $(`#date`).text(date);
-  $(`#number`).text(luckyNum);
-  $(`#mood`).text(mood);
+//   $(`#color`).text(color);
+//   $(`#compatibility`).text(compatibility);
+//   $(`#time`).text(luckyTime);
+//   $(`#description`).text(description);
+//   $(`#date`).text(date);
+//   $(`#number`).text(luckyNum);
+//   $(`#mood`).text(mood);
   
-  const colorPromise = Movie.fetchData(color)
-  const compatibilityPromise = Movie.fetchData(compatibility)
-  const moodPromise = Movie.fetchData(mood)
-  Promise.all([colorPromise, compatibilityPromise, moodPromise]).then(function(promiseResponse) {
-    let movieArray = []
-    console.log('hello', promiseResponse)
-    for (let movies of promiseResponse) {
-      console.log("movis", movies)
-      movieArray.push(movies)
-      console.log('movie index', movies)
-      for (const movie of movieArray) {
-        console.log("inner movies", movieArray)
-        $('.movieResults').append(`<img src="${movie.data.d.i.imageUrl}">`);
-      }
-    }
-    console.log("movie array", promiseArray);
-    $('.movieResults').html(firstMovieArray);
-  });
+// const colorPromise = Movie.fetchData(color);
+// const compatibilityPromise = Movie.fetchData(compatibility);
+// const moodPromise = Movie.fetchData(mood);
+// Promise.all([colorPromise, compatibilityPromise, moodPromise]).then(function(promiseResponse) {
+//   let movieArray = [];
+//   console.log('hello', promiseResponse);
+//   for (let movies of promiseResponse) {
+//     console.log("movis", movies);
+//     movieArray.push(movies);
+//     console.log('movie index', movies);
+//     for (const movie of movieArray) {
+//       console.log("inner movies", movieArray);
+//       $('.movieResults').append(`<img src="${movie.data.d.i.imageUrl}">`);
+//     }
+//   }
+//   console.log("movie array", promiseArray);
+//   $('.movieResults').html(firstMovieArray);
+// });
 
-}
-
-
-
-// color, compatibility & mood
-// get movie elements data[0].d
-
-// let getColorCompatMood = async function(color) {
-  // let color = getZodiacElements(color)
-  // let compatability =  getZodiacElements(compatability)
-  // let mood =  getZodiacElements(mood)
-
-  // Promise.all(color, compatability, mood).then
-  
-  // console.log("for movie2", data2);
-  // console.log("for movie1", data1);
-
-
-//getColorCompatMood(color);
-
-
-
-
-
-// let data = Zodiac.fetchData('Virgo')
-
-// for (res in data) {
-//   console.log("display objects", res);
 // }
 
 
-//let data1 = Zodiac.fetchData('Virgo')
-//console.log("description", data1);
-
-
-//getZodiacElements('Virgo')
 
 let createImg = (sign) => {
   let imgElement = document.createElement('img');
@@ -98,8 +65,31 @@ $(document).ready(function() {
   $('.form').on('submit', (e) => {
     e.preventDefault();
     let zodiacSelect = $('#zodiacSelect').val();
-    console.log('zodiac input', zodiacSelect);
-    getZodiacElements(zodiacSelect);
+    // getZodiacElements(zodiacSelect);
+    Zodiac.fetchData(zodiacSelect)
+      .then(response => {
+        if(response instanceof Error) {
+          throw Error (`Zodiac API error: ${response.message}`);
+        }
+        console.log('hello', response);
+        let color = response.data.color;
+        console.log('red', color);
+        let compatibility = response.data.compatibility;
+        let luckyTime = response.data.lucky_time;
+        let description = response.data.description;
+        let date = response.data.date_range;
+        let luckyNum = response.data.lucky_number;
+        let mood = response.data.mood;
+      
+        $(`#color`).text(color);
+        $(`#compatibility`).text(compatibility);
+        $(`#time`).text(luckyTime);
+        $(`#description`).text(description);
+        $(`#date`).text(date);
+        $(`#number`).text(luckyNum);
+        $(`#mood`).text(mood);
+      });
+
     $('.form').hide();
     let img = createImg(zodiacSelect.toLowerCase());
     $('.zodiacResults').prepend(img);
